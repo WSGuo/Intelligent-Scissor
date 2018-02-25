756e88ecbfd2db27c9ac75a04f6bfe69cadd2388
Guo <wguoaa@connect.ust.hk>
2018-02-25 16:56:22 +0800

    add contour.m

---
+++ project1/contour.m
@@ -0,0 +1,18 @@
+%return positions (in the original image) of pixels in the contour
+%input are positions in original image
+function contourArray = contour(seedRow, seedCol, curRow, curCol,costGraph)
+disp('start calculate contour');
+nodeMat = LiveWireDP(-1, seedCol, seedRow, costGraph);
+cur = nodeMat(curRow-1, curCol-1);
+contourArray = [curRow curCol];
+while isa(cur.prevNode,'Node')
+    cur = cur.prevNode;
+    contourArray = [contourArray;[cur.row+1 cur.col+1]];
+end
+%check if the end Node is the seed Node
+if (cur.row+1)~= seedRow || (cur.col+1 ~= seedCol)
+    disp('error: contour end is not at seed!')
+end
+disp('finish calculate contour')
+end
+    
